02dea62d79b60adfd9928c27ba801e6a0d24b82a
SURFACE_PRO4\ken <tomorrow.is.another.day.246@gmail.com>
2017-06-13 01:47:08 +0900

    add solution

---
+++ VibrateSample/VibrateSample/VibrateSample/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Plugin.Vibrate;
+using System;
+using Xamarin.Forms;
+
+namespace VibrateSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_Clicked(object sender, EventArgs e)
+        {
+            var v = CrossVibrate.Current;
+            v.Vibration(100);
+            await System.Threading.Tasks.Task.Delay(100);
+            v.Vibration(200);
+        }
+    }
+}
